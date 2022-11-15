27e768d86120a42dad0b1e868fbfda56f865270f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-11-15 10:39:40 +0530

    programming-fundamentals

--- src/com/deloitte/demo/SwitchDemo.java
+++ src/com/deloitte/demo/SwitchDemo.java
@@ -8,7 +8,7 @@ public class SwitchDemo {
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter num:");
+		System.out.println("Enter num:"); 
 		int num = sc.nextInt();
 
 		switch (num) {
