501364a14d0a3a4fc9fb6754bf585586b665a314
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-11-15 16:05:09 +0530

    programming-fundamentals

--- src/com/deloitte/demo/oop/ab/AbstractDemo.java
+++ src/com/deloitte/demo/oop/ab/AbstractDemo.java
@@ -2,4 +2,11 @@ package com.deloitte.demo.oop.ab;
 
 public class AbstractDemo {
 
+	// a class can extend only one another class
+	// a class can implement one or more interfaces
+	// a class can do both of the above
+	// abstract method has only method signature
+	// it can be created only in an abstract class or in an interface
+	// abstract method need to be implemented in a concrete class to use it
+
 }
