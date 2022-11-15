1e7a6f80e77f5f1146f6d5b50cb22a5a44521ffb
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-11-15 10:58:45 +0530

    programming-fundamentals

---
+++ src/com/deloitte/demo/Employee.java
@@ -0,0 +1,13 @@
+package com.deloitte.demo;
+
+public class Employee {
+
+	int eid;
+	String firstName;
+	double salary;
+
+	void work() {
+		System.out.println(firstName + " works...");
+	}
+
+}
---
+++ src/com/deloitte/demo/EmployeeDemo.java
@@ -0,0 +1,21 @@
+package com.deloitte.demo;
+
+public class EmployeeDemo {
+	
+	public static void main(String[] args) {
+		
+		Employee emp = new Employee();
+		
+		emp.eid = 101;
+		emp.firstName = "Sonu";
+		emp.salary = 90000;
+		
+		emp.work();
+
+
+		
+		
+	}
+	
+
+}
