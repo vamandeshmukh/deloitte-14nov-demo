90427c8cc49daefce6e052311ce369eb44ed4030
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-11-15 12:38:10 +0530

    programming-fundamentals

--- src/com/deloitte/demo/cons/ConstructorDemo.java
+++ src/com/deloitte/demo/cons/ConstructorDemo.java
@@ -1,5 +1,18 @@
 package com.deloitte.demo.cons;
 
+/*
+ * Constructor - 
+ * - a special type of method 
+ * - needed to create objects 
+ * - name is same as class name 
+ * - does not have return type
+ * - available by default 
+ * - can also be created manually 
+ * - if we create our constructors, 
+ * - then default constructor is not available 
+ * - one class can have one or more constructors  
+ */
+
 public class ConstructorDemo {
 
 	public static void main(String[] args) {
