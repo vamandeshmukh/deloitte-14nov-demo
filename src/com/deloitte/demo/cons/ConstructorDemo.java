ef1fcf0379f9e40fdfaa6642a96d4c1aaafeeff7
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-11-15 12:15:19 +0530

    programming-fundamentals

--- src/com/deloitte/demo/cons/ConstructorDemo.java
+++ src/com/deloitte/demo/cons/ConstructorDemo.java
@@ -2,4 +2,19 @@ package com.deloitte.demo.cons;
 
 public class ConstructorDemo {
 
+	public static void main(String[] args) {
+
+		Employee obj = new Employee();
+		obj.eid = 101;
+		obj.firstName = "Sonu";
+		obj.salary = 90000;
+		System.out.println(obj.toString());
+
+		Employee obj2 = new Employee();
+		obj2.eid = 102;
+		obj2.firstName = "Monu";
+		obj2.salary = 95000;
+		System.out.println(obj2.toString());
+	}
+
 }
